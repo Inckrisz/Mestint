d12c9f0322da8f36db599c53b37cf6f3c1ecf713
Inckrisz <125383357+Inckrisz@users.noreply.github.com>
2025-03-03 19:40:10 +0100

    Create hanoi.py

---
+++ hanoi.py
@@ -0,0 +1,17 @@
+class Hanoi_problema:
+    def __init__(self, ke, c):
+        self.kezdo = ke
+        self.cel = c
+
+    def celteszt(self, allapot):
+        return allapot == self.cel
+
+    def rakovetkezo(self,allapot):
+        gyerekek = list()
+
+
+        return gyerekek
+
+
+if __name__ == "__main__":
+    h = Hanoi_problema(("P","P","P"), ("R","R","R"))
