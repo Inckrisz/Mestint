fed4df52082fbfe1537222ff02ccc6532e22438d
Inckrisz <125383357+Inckrisz@users.noreply.github.com>
2025-03-10 19:44:36 +0100

    Update hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -6,9 +6,27 @@ class Hanoi_problema:
     def celteszt(self, allapot):
         return allapot == self.cel
 
-    def rakovetkezo(self,allapot):
+    def rakovetkezo(self,a):
         gyerekek = list()
 
+        for melyiket in range (0,3):
+            for hova in ["P","Q","R"]:
+                alkalmazhato = True
+                if a[melyiket] != hova:
+                    for i in range (0, melyiket):
+                        if a[i] != a[melyiket] and a[i] != hova:
+                            alkalmazhato = True
+                        else:
+                            alkalmazhato = False
+                            break
+                else:
+                    alkalmazhato = False
+
+                if alkalmazhato:
+                    tmp = list(a)
+                    tmp[melyiket] = hova
+                    uj_allapot = tuple(tmp)
+                    gyerekek.append(("operator HF", uj_allapot))
 
         return gyerekek
 
