817c9a1b2036594c5773f311f905b9c196fb571c
Inckrisz <125383357+Inckrisz@users.noreply.github.com>
2025-03-31 16:31:32 +0200

    Update n_kiralyno.py

--- n_kiralyno.py
+++ n_kiralyno.py
@@ -1,19 +1,22 @@
-class n_kiralyno_problema:
-    def __init__(self, k, c):
+from keres import Feladat,szélességi_fakeresés
+
+
+class n_kiralyno_problema(Feladat):
+    def __init__(self,k,c):
         self.kezdő = k
-        self.cél = c
-        self.N = len(k) - 1
+        self.cél  = c
+        self.N = len(k) -1
 
-    def célteszt (self, a):
-        return a[self.N] == self.cél
+    def célteszt(self,a):
+        return a[self.N]  == self.cél
 
-    def rákövetkező (self, a):
+    def rákövetkező(self,a):
         gyereke = []
-        s = a[self.N]
+        s= a[self.N]
 
-        for j in range(0, self.N):
+        for j in range(0,self.N):
             alkalmazhato = True
-            for m in range (0, s):
+            for m in range(0,s):
                 if a[m] != j and abs(m-s) != abs(a[m]-j):
                     #alkalmazhato = True
                     pass
@@ -24,15 +27,26 @@ class n_kiralyno_problema:
             if alkalmazhato:
                 tmp = list(a)
                 tmp[s] = j
-                tmp[self.N] = s + 1
+                tmp[self.N] = s+1
                 uj_allapot = tuple(tmp)
-                gyereke.append ((s+" -> "+j, uj_allapot))
-
+                gyereke.append((str(s)+" -> "+str(j),uj_allapot))
 
         return gyereke
 
 
 
 
-if __name__=="__main__":
-    h = n_kiralyno_problema((-1,-1,-1,-1,-1,-1,-1,-1,0), 8)
+
+
+
+
+
+if __name__ == "__main__":
+    h = n_kiralyno_problema((-1,-1,-1,-1,-1,-1,-1,-1,0),8)
+
+    csucs = szélességi_fakeresés(h)
+
+    megoldas = csucs.út()
+    megoldas.reverse()
+
+    print(megoldas)
