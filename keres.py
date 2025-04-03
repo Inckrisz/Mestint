101cb272f6f414edb042bb051a60dbb677b8aa8a
Inckrisz <125383357+Inckrisz@users.noreply.github.com>
2025-04-03 22:03:07 +0200

    Update keres.py

--- keres.py
+++ keres.py
@@ -5,8 +5,8 @@ from seged import *
 class Feladat:
 
     def __init__(self, kezdő, cél=None):
-        self.kezdő = kezdő
-        self.cél = cél
+        self.kezdő = tuple(kezdő)  # Lista helyett tuple
+        self.cél = tuple(cél) if cél else None
 
     def rákövetkező(self, állapot):
         raise NotImplementedError
@@ -15,8 +15,7 @@ class Feladat:
         raise NotImplementedError
 
     def célteszt(self, állapot):
-       #return állapot == self.cél
-       raise NotImplementedError
+        return állapot == self.cél
 
     def útköltség(self, c, állapot1, lépés, állapot2):
         return c + 1
@@ -25,7 +24,7 @@ class Feladat:
 class Csúcs:
 
     def __init__(self, állapot, szülő=None, lépés=None, útköltség=0):
-        self.állapot = állapot
+        self.állapot = tuple(állapot)  # Lista helyett tuple
         self.szülő = szülő
         self.lépés = lépés
         self.útköltség = útköltség
@@ -36,8 +35,6 @@ class Csúcs:
 
     def __repr__(self):
         return "<Csúcs: %s>" % (self.állapot, )
-
-        #return "%s" % (list(self.állapot),)
 
     def út(self):
         x, válasz = self, [self]
@@ -53,36 +50,32 @@ class Csúcs:
 
     def kiterjeszt(self, feladat):
         for (művelet, következő) in feladat.rákövetkező(self.állapot):
-            if következő not in [csúcs.állapot for csúcs in self.út()]:
-                yield Csúcs(következő, self, művelet,
-                            feladat.útköltség(self.útköltség, self.állapot, művelet,                                              következő))
+            if tuple(következő) not in [csúcs.állapot for csúcs in self.út()]:
+                yield Csúcs(tuple(következő), self, művelet,
+                            feladat.útköltség(self.útköltség, self.állapot, művelet,
+                                              következő))
+
 
 def fakereső(feladat, perem):
-    perem.append(Csúcs(feladat.kezdő))
+    perem.append((Csúcs(feladat.kezdő)))
     while perem:
         csúcs = perem.pop()
         if feladat.célteszt(csúcs.állapot):
-            return  csúcs
+            return csúcs
         else:
             perem.extend(csúcs.kiterjeszt(feladat))
-
+            
     return None
 
 
-def szélességi_fakeresés(feladat):
-    return fakereső(feladat, Sor())
-
-def mélységi_fakeresés(feladat):
-    return fakereső(feladat,Verem())
-
-
 def gráfkereső(feladat, perem):
-    perem.append(Csúcs(feladat.kezdő))
+    perem.append((Csúcs(feladat.kezdő)))
     kifejtési_sor = set()
     while perem:
         csúcs = perem.pop()
         if feladat.célteszt(csúcs.állapot):
             return csúcs
+
         if csúcs.állapot not in kifejtési_sor:
             kifejtési_sor.add(csúcs.állapot)
             perem.extend(csúcs.kiterjeszt(feladat))
@@ -91,11 +84,27 @@ def gráfkereső(feladat, perem):
 
 
 def szélességi_gráfkereső(feladat):
+    return gráfkereső(feladat, Sor())
 
-    return  gráfkereső(feladat, Sor())
 
 def mélységi_gráfkereső(feladat):
-
     return gráfkereső(feladat, Verem())
 
 
+def szélességi_fakereső(feladat):
+    return fakereső(feladat, Sor())
+
+
+def mélységi_fakereső(feladat):
+    return fakereső(feladat, Verem())
+
+
+def best_first(feladat, h):
+    return gráfkereső(feladat, RendezettLista(h))
+
+
+def a_csillag(feladat, h):
+    def f(n):
+        return n.útköltség + h(n)
+
+    return best_first(feladat, f)
